895dfa6fa6e9bd155d354ea67374905d63ec62ad
Junior <1344065492@qq.com>
2021-11-13 20:43:36 +0800

    frameworktest

---
+++ src/main/java/cn/szuer/publicboard/PublicboardApplication.java
@@ -0,0 +1,13 @@
+package cn.szuer.publicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/szuer/publicboard/PublicboardApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.szuer.publicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
