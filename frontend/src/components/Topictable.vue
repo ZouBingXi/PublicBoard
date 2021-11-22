e9832a87a0b34fc7682a10f4a92006d831e44337
邹秉禧 <zoubingxi@noreply21929541.tapd.cn>
2021-11-22 14:36:13 +0000

    Frontend built framework

---
+++ frontend/src/components/Topictable.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Topictable"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-header>
+    <el-menu mode="horizontal">
+      <div style="
+        margin-left: 1%;
+        margin-right: 1%;
+        font-size: 24px;
+        float: left;
+        line-height: 60px;
+        color: black">
+        <span>PublicBoard主页</span>
+      </div>
+      <el-menu-item style="float: right">sign out</el-menu-item>
+    </el-menu>
+    <h1 style="vertical-align: center;color: white"></h1>
+  </el-header>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader";
+export default {
+  name: "Home",
+  components: {TheHeader},
+}
+</script>
+
+<style scoped>
+
+</style>
