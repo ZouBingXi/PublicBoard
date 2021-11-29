6c2b097a91881e3bfed59acaecb3f78ca2baa845
hzh <1564068065@qq.com>
2021-11-29 17:07:17 +0800

    ff #Finished ff

--- src/main/java/cn/szuer/publicboard/service/MyUserDetailsService.java
+++ src/main/java/cn/szuer/publicboard/service/MyUserDetailsService.java
@@ -3,7 +3,6 @@ package cn.szuer.publicboard.service;
 import cn.szuer.publicboard.mapper.UserInfoMapper;
 import cn.szuer.publicboard.mapper.UserTypeMapper;
 import cn.szuer.publicboard.model.UserInfo;
-import cn.szuer.publicboard.model.UserType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
